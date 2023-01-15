975d2eed49d874e191a5ea92cfbb00ad72e8b023
pesua <anton.chernetskiy@getgsi.com>
2023-01-15 21:07:26 +0200

    Add german writers

--- datagrab.py
+++ datagrab.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import csv
+import string
 
 wikipedia = MediaWiki()
 
@@ -11,38 +12,44 @@ def backlinks(name):
     p = wikipedia.page(name)
     return p.backlinks
 
-# backlinks('List of English writers (A–C)')
-
 
 session = requests.Session()
 
-URL = 'https://en.wikipedia.org/wiki/'
 
-
-def grab_authors(author):
-    url = URL + author.replace(' ', '_')
-    response = session.get(url=url)
+def grab_authors(page, c_code):
+    print('Parsing ' + page)
+    url = 'https://' + c_code + '.wikipedia.org/wiki/'
+    response = session.get(url + page.replace(' ', '_'))
     soup = BeautifulSoup(response.text, "html.parser")
-
-    return {author.a['title']: author.a['href'] for author in soup.select('.div-col li') if author.a}
-
-
-english_authors = ['List of English writers (A–C)', 'List of English writers (D–J)',
-                   'List of English writers (K–Q)', 'List of English writers (R–Z)']
-# authors = grab_authors(english_authors[0])
-
-authors = {}
-for page in english_authors:
-    authors.update(grab_authors(page))
-    time.sleep(2)
+    if c_code == 'en' :
+        return {author.a['title']: author.a['href'] for author in soup.select('.div-col li') if author.a}
+    elif c_code == 'de':
+        return {author['title']: author['href'] for author in soup.select('dd a')}
 
 
-f = open('english_writers.csv', 'w')
+def parce_autors(c_code, list_pages):
+    authors = {}
+    for page in list_pages:
+        authors.update(grab_authors(page, c_code))
+        time.sleep(2)
+    return authors
 
-writer = csv.writer(f)
-writer.writerow(['name', 'path'])
-for key in sorted(authors):
-    writer.writerow([key, authors[key]])
-f.close()
+
+def store(c_code, authors):
+    f = open(c_code + '_writers.csv', 'w')
+    writer = csv.writer(f)
+    writer.writerow(['name', 'path', 'domain'])
+    for key in sorted(authors):
+        writer.writerow([key, authors[key], c_code])
+    f.close()
+
+
+# list_pages = ['List of English writers (A–C)', 'List of English writers (D–J)',
+#                    'List of English writers (K–Q)', 'List of English writers (R–Z)']
+# c_code = 'en'
+list_pages = ['Liste_von_Autoren/' + letter for letter in list(string.ascii_uppercase)]
+c_code = 'de'
+authors = parce_autors(c_code, list_pages)
+store(c_code, authors)
 
 print(len(authors))
