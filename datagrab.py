281fc8785f37fd981986932491a140346d359412
Artem Loginov <artem.loginov@audienzz.ch>
2023-02-20 14:22:41 +0200

    Add french parser. Add parsed files to repo. Add git ignore.

--- datagrab.py
+++ datagrab.py
@@ -1,4 +1,4 @@
-from mediawiki import MediaWiki
+# from mediawiki import MediaWiki
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -19,9 +19,12 @@ session = requests.Session()
 def grab_authors(page, c_code):
     print('Parsing ' + page)
     url = 'https://' + c_code + '.wikipedia.org/wiki/'
-    response = session.get(url + page.replace(' ', '_'))
+    'https://fr.wikipedia.org/wiki/Liste_d%27%C3%A9crivains_de_langue_fran%C3%A7aise_par_ordre_chronologique'
+    'https://fr.wikipedia.org/wiki/Liste_d%27%C3%A9crivains_de_langue_fran%C3%A7aise_par_ordre_chronologique'
+    replace___ = url + page.replace(' ', '_')
+    response = session.get(replace___)
     soup = BeautifulSoup(response.text, "html.parser")
-    if c_code == 'en' :
+    if c_code == 'en':
         return {author.a['title']: author.a['href'] for author in soup.select('.div-col li') if author.a}
     elif c_code == 'de':
         return {author['title']: author['href'] for author in soup.select('dd a')}
@@ -47,8 +50,10 @@ def store(c_code, authors):
 # list_pages = ['List of English writers (A–C)', 'List of English writers (D–J)',
 #                    'List of English writers (K–Q)', 'List of English writers (R–Z)']
 # c_code = 'en'
-list_pages = ['Liste_von_Autoren/' + letter for letter in list(string.ascii_uppercase)]
-c_code = 'de'
+# list_pages = ['Liste_von_Autoren/' + letter for letter in list(string.ascii_uppercase)]
+# c_code = 'de'
+list_pages = ['Liste_d%27%C3%A9crivains_de_langue_fran%C3%A7aise_par_ordre_chronologique']
+c_code = 'fr'
 authors = parce_autors(c_code, list_pages)
 store(c_code, authors)
 
